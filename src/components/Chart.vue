7a15361c9972c781ee48aadc4af35c6f24397290
n0312149 <Alex.Yu@libertymutual.com>
2020-04-06 00:25:28 -0400

    element-ui for frontend. Select and Charting component added

---
+++ src/components/Chart.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-row justify="center">
+        <el-header>
+            <el-row justify="center"><h1>{{ state? state : "US" }} Covid-19 Chart</h1></el-row>
+        </el-header>
+        <p>here</p>
+    </el-row>
+</template>
+
+<script>
+    export default {
+        name: 'Chart',
+        props: {
+            state: String
+        }
+    }
+</script>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,39 +1,96 @@
 <template>
-  <div class="landing">
-    <h1>{{ msg }}</h1>
-    <p>
-      Select your state:
-    </p>
-    <label for="state"></label><select id="state">
-      <option value="MA">Massachusetts</option>
-    </select>
-    <h3>Chart</h3>
-  </div>
+  <el-container>
+    <el-row justify="center">
+      <el-header>
+        <el-row justify="center"><h1>{{ msg }}</h1></el-row>
+      </el-header>
+      <el-row justify="center">
+        <el-select v-model="selectedState" placeholder="Select">
+          <el-option
+                  v-for="item in stateOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+          </el-option>`
+        </el-select>
+      </el-row>
+      <br/>
+      <el-row justify="center">
+        <Chart :state=this.selectedState />
+      </el-row>
+    </el-row>
+  </el-container>
 </template>
 
 <script>
-export default {
-  name: 'LandingPage',
-  props: {
-    msg: String
-  }
+  import Chart from './Chart.vue'
+
+  export default {
+    name: 'LandingPage',
+    components: {
+      Chart
+    },
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        selectedState: "US",
+        stateOptions: [
+          { label: 'United States', value: 'US'},
+          { label: 'ALABAMA', value: 'AL'},
+          { label: 'ALASKA', value: 'AK'},
+          { label: 'ARIZONA', value: 'AZ'},
+          { label: 'ARKANSAS', value: 'AR'},
+          { label: 'CALIFORNIA', value: 'CA'},
+          { label: 'COLORADO', value: 'CO'},
+          { label: 'CONNECTICUT', value: 'CT'},
+          { label: 'DELAWARE', value: 'DE'},
+          { label: 'FLORIDA', value: 'FL'},
+          { label: 'GEORGIA', value: 'GA'},
+          { label: 'HAWAII', value: 'HI'},
+          { label: 'IDAHO', value: 'ID'},
+          { label: 'ILLINOIS', value: 'IL'},
+          { label: 'INDIANA', value: 'IN'},
+          { label: 'IOWA', value: 'IA'},
+          { label: 'KANSAS', value: 'KS'},
+          { label: 'KENTUCKY', value: 'KY'},
+          { label: 'LOUISIANA', value: 'LA'},
+          { label: 'MAINE', value: 'ME'},
+          { label: 'MARYLAND', value: 'MD'},
+          { label: 'MASSACHUSETTS', value: 'MA'},
+          { label: 'MICHIGAN', value: 'MI'},
+          { label: 'MINNESOTA', value: 'MN'},
+          { label: 'MISSISSIPPI', value: 'MS'},
+          { label: 'MISSOURI', value: 'MO'},
+          { label: 'MONTANA', value: 'MT'},
+          { label: 'NEBRASKA', value: 'NE'},
+          { label: 'NEVADA', value: 'NV'},
+          { label: 'NEW HAMPSHIRE', value: 'NH'},
+          { label: 'NEW JERSEY', value: 'NJ'},
+          { label: 'NEW MEXICO', value: 'NM'},
+          { label: 'NEW YORK', value: 'NY'},
+          { label: 'NORTH CAROLINA', value: 'NC'},
+          { label: 'NORTH DAKOTA', value: 'ND'},
+          { label: 'OHIO', value: 'OH'},
+          { label: 'OKLAHOMA', value: 'OK'},
+          { label: 'OREGON', value: 'OR'},
+          { label: 'PENNSYLVANIA', value: 'PA'},
+          { label: 'PUERTO RICO', value: 'PR'},
+          { label: 'RHODE ISLAND', value: 'RI'},
+          { label: 'SOUTH CAROLINA', value: 'SC'},
+          { label: 'SOUTH DAKOTA', value: 'SD'},
+          { label: 'TENNESSEE', value: 'TN'},
+          { label: 'TEXAS', value: 'TX'},
+          { label: 'UTAH', value: 'UT'},
+          { label: 'VERMONT', value: 'VT'},
+          { label: 'VIRGINIA', value: 'VA'},
+          { label: 'WASHINGTON', value: 'WA'},
+          { label: 'WEST VIRGINIA', value: 'WV'},
+          { label: 'WISCONSIN', value: 'WI'},
+          { label: 'WYOMING', value: 'WY' }
+        ]
+      }
+    }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
