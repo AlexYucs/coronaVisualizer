546e0903e4e11ce2b4eed8c82f1ca0d5dc02a2ad
n0312149 <Alex.Yu@libertymutual.com>
2020-04-11 21:18:18 -0400

    search by state abb

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,15 @@
 <template>
   <el-container>
     <el-row justify="center">
-        <el-select label="State" v-model="selectedState" placeholder="Select" filterable no-match-text="No matching data">
+        <el-select
+                label="State"
+                v-model="selectedState"
+                placeholder="Select"
+                filterable
+                no-match-text="No matching data"
+                :filter-method="filterSearch"
+                @visible-change	="filterReset"
+        >
           <el-option
                   v-for="item in stateOptions"
                   :key="item.value"
@@ -24,11 +32,74 @@
     components: {
       Chart
     },
+    methods:{
+      filterSearch(input){
+        const formattedInput = input.toUpperCase();
+        this.stateOptions = this.stateList.filter(state => state.label.includes(formattedInput) || state.value === formattedInput);
+      },
+      filterReset(){
+        console.log(JSON.stringify("resetting", null, 2));
+        this.stateOptions = this.stateList;
+      }
+    },
     props: {
     },
     data() {
       return {
         selectedState: "AL",
+        stateList: [
+          { label: 'ALABAMA', value: 'AL'},
+          { label: 'ALASKA', value: 'AK'},
+          { label: 'ARIZONA', value: 'AZ'},
+          { label: 'ARKANSAS', value: 'AR'},
+          { label: 'CALIFORNIA', value: 'CA'},
+          { label: 'COLORADO', value: 'CO'},
+          { label: 'CONNECTICUT', value: 'CT'},
+          { label: 'DELAWARE', value: 'DE'},
+          { label: 'FLORIDA', value: 'FL'},
+          { label: 'GEORGIA', value: 'GA'},
+          { label: 'HAWAII', value: 'HI'},
+          { label: 'IDAHO', value: 'ID'},
+          { label: 'ILLINOIS', value: 'IL'},
+          { label: 'INDIANA', value: 'IN'},
+          { label: 'IOWA', value: 'IA'},
+          { label: 'KANSAS', value: 'KS'},
+          { label: 'KENTUCKY', value: 'KY'},
+          { label: 'LOUISIANA', value: 'LA'},
+          { label: 'MAINE', value: 'ME'},
+          { label: 'MARYLAND', value: 'MD'},
+          { label: 'MASSACHUSETTS', value: 'MA'},
+          { label: 'MICHIGAN', value: 'MI'},
+          { label: 'MINNESOTA', value: 'MN'},
+          { label: 'MISSISSIPPI', value: 'MS'},
+          { label: 'MISSOURI', value: 'MO'},
+          { label: 'MONTANA', value: 'MT'},
+          { label: 'NEBRASKA', value: 'NE'},
+          { label: 'NEVADA', value: 'NV'},
+          { label: 'NEW HAMPSHIRE', value: 'NH'},
+          { label: 'NEW JERSEY', value: 'NJ'},
+          { label: 'NEW MEXICO', value: 'NM'},
+          { label: 'NEW YORK', value: 'NY'},
+          { label: 'NORTH CAROLINA', value: 'NC'},
+          { label: 'NORTH DAKOTA', value: 'ND'},
+          { label: 'OHIO', value: 'OH'},
+          { label: 'OKLAHOMA', value: 'OK'},
+          { label: 'OREGON', value: 'OR'},
+          { label: 'PENNSYLVANIA', value: 'PA'},
+          { label: 'PUERTO RICO', value: 'PR'},
+          { label: 'RHODE ISLAND', value: 'RI'},
+          { label: 'SOUTH CAROLINA', value: 'SC'},
+          { label: 'SOUTH DAKOTA', value: 'SD'},
+          { label: 'TENNESSEE', value: 'TN'},
+          { label: 'TEXAS', value: 'TX'},
+          { label: 'UTAH', value: 'UT'},
+          { label: 'VERMONT', value: 'VT'},
+          { label: 'VIRGINIA', value: 'VA'},
+          { label: 'WASHINGTON', value: 'WA'},
+          { label: 'WEST VIRGINIA', value: 'WV'},
+          { label: 'WISCONSIN', value: 'WI'},
+          { label: 'WYOMING', value: 'WY' }
+        ],
         stateOptions: [
           { label: 'ALABAMA', value: 'AL'},
           { label: 'ALASKA', value: 'AK'},
